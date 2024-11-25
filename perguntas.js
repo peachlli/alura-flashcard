1359d8cdd14519834c0ae595b9ddc97bf995389d
peachlli <00001102600945sp@al.educacao.sp.gov.br>
2024-11-25 17:56:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Roteiro',
+    'Quem escreveu o roteiro do filme Coraline?',
+    'O roteiro foi adaptado por Henry Selick, baseado no livro de Neil Gaiman.'
+)
+
+criaCartao(
+    'Direção',
+    'Quem dirigiu o filme Coraline?',
+    ' O filme foi dirigido por Henry Selick.'
+)
+
+criaCartao(
+    'Animação',
+    'Qual tipo de animação foi usado em Coraline?',
+    'Coraline foi feito com animação em stop-motion.'
+)
+
+criaCartao(
+    'Música',
+    'Quem compôs a trilha sonora de Coraline?',
+    'A trilha sonora foi composta por Bruno Coulais.'
+)
